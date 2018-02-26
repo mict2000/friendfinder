802757b45d35a3fb0213a16a6fc031d98361dd83
Michael Thompson <mict2000@yahoo.com>
2018-02-25 17:12:16 -0800

    Init commit files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Mike Thompson',
+    profilePic: 'https://pbs.twimg.com/profile_images/818579999766683648/9qJ2T35c_400x400.jpg',
+    scores: [
+      "3",
+      "5",
+      "4",
+      "5",
+      "5",
+      "1",
+      "1",
+      "1",
+      "3",
+      "5"
+    ]
+  }
+]
+
+module.exports = friendList;
